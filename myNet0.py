25e8d205c148a841edbf9b2f8a844e390c8cf799
mckim2020 <mckim2013@gmail.com>
2020-08-07 17:19:58 +0900

    Update myNet0.py

--- myNet0.py
+++ myNet0.py
@@ -1,28 +1,55 @@
 import torch
-  1 import torch.nn as nn
-  2 import torch.nn.functional as F
-  3
-  4 import torch.optim as optim
-  5
-  6 """Create Net"""
-  7 class Net(nn.Module):
-  8     def __init__(self):
-  9         super(Net, self).__init__()
- 10         """Kernel"""
- 11         self.conv1 = nn.Conv2d(1, 6, 5)
- 12         self.pool = nn. MaxPool2d(2, 2)
- 13         self.conv2 = nn.Conv2d(6, 16, 5)
- 14
- 15         """Output"""
- 16         self.fc1 = nn.Linear(16 * 5 * 5, 120)
- 17         self.fc2 = nn.Linear(120, 84)
- 18         self.fc3 = nn.Linear(84, 10)
- 19
- 20     def forward(self, x):
- 21         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
- 22         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
- 23         x = x.view(-1, 16 * 5 * 5)
- 24         x = F.relu(self.fc1(x))
- 25         x = F.relu(self.fc2(x))
- 26         x = self.fc3(x)
- 27         return x
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+
+"""Create Net"""
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        """Kernel"""
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn. MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        """Output"""
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+     """ num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+    """
+
+
+net = Net()
+input = torch.randn(1, 1, 32, 32)
+target = torch.randn(10)
+target = target.view(1, -1)
+
+"""Create Loss Optimizer(SGD)"""
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.MSELoss()
+
+
+  
+"""Code under has to be implemented inside training loop"""
+
+"""Zero the grad buffers(Initializing)"""
+optimizer.zero_grad()
+output = net(input)
+loss = criterion(output, target)
+loss.backward()
